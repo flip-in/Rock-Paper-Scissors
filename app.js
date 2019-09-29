dc6232c0c3a3da6e980f26f91f514dcb2e3a11a9
Will Price <pricew86@gmail.com>
2019-09-29 18:25:31 +0900

    added stuff

--- app.js
+++ app.js
@@ -4,15 +4,16 @@ const game = () => {
 
     //start the game
     const startGame = () =>{
-        const playBtn = document.querySelector('.intro button');
+        //const playBtn = document.querySelector('.intro button');
         const introScreen = document.querySelector('.intro');
         const matchScreen = document.querySelector('.match');
         const scoreScreen = document.querySelector('.score');
-        playBtn.addEventListener('click', ()=> {
+        document.getElementById("intro-button").addEventListener('click', function clicked() {
             introScreen.classList.add('fadeOut');
             matchScreen.classList.add('fadeIn');
             scoreScreen.classList.add('fadeIn');
-        });
+            document.getElementById("intro-button").removeEventListener("click", clicked, false);
+        }, false);
     };
     //Play Match
     const playMatch = () =>{
@@ -20,7 +21,59 @@ const game = () => {
         const playerHand = document.querySelector('.player-hand');
         const computerHand = document.querySelector('.computer-hand');
         const hands = document.querySelectorAll('.hands img');
-        
+
+
+     function myListener(e){
+            //console.log(this);
+            //The Computer Choice
+            const computerNumber = Math.floor(Math.random() *3);
+            const computerChoice = computerOptions[computerNumber];
+            //Timeout takes function (first) and time (second)
+            setTimeout(() =>{
+                //Here is where we call compareHands -- needs delay
+                compareHands(this.textContent, computerChoice);
+                //Update Images -- needs delay
+                playerHand.src = `./assets/${this.textContent}.png`
+                computerHand.src = `./assets/${computerChoice}.png`
+                //reset button text to white -- needs delay
+                this.style.color = "white";
+                //re-enable buttons -- needs delay
+                options.forEach(el =>{el.disabled = false;});
+                console.log("computer score:   ", cScore);
+                console.log("player Score:     ", pScore);
+                //Check End Game Trigger
+                if(cScore > 1 || pScore >1){
+                    console.log("Game Over!");
+                    options.forEach(option=>{
+                        myRemoveEvent(option, "click", myListener);
+                        console.log("removed");
+                        console.log(option);
+                    })
+                    endGame();
+                }
+            }, 2000)
+            //Shaking animation no delay
+            playerHand.style.animation = "shakePlayer 2s ease";
+            computerHand.style.animation = "shakeComputer 2s ease";
+            //current button color is red no delay
+            this.style.color = "rgb(39, 41, 68)";
+            //disable all buttons no delay
+            options.forEach(el => {el.disabled = true;
+            });
+            
+        };
+        function myAttachEvent(element, type, handler){
+            if (element.addEventListener) 
+                element.addEventListener(type, handler, false);
+            else element.attachEvent("on"+type, handler);
+        }
+        function myRemoveEvent(element,type,handler) {
+            if (element.removeEventListener) 
+               element.removeEventListener (type,handler,false);
+            if (element.detachEvent)
+               element.detachEvent ('on'+type,handler); 
+        }
+
         hands.forEach(hand =>{
             hand.addEventListener('animationend', function(){
                 this.style.animation = '';
@@ -30,44 +83,10 @@ const game = () => {
         const computerOptions = ['rock', 'paper', 'scissors'];
         
         options.forEach(option=>{
-            
-            option.addEventListener('click', function(){
-                console.log(this);
-                //The Computer Choice
-                const computerNumber = Math.floor(Math.random() *3);
-                const computerChoice = computerOptions[computerNumber];
-                //Timeout takes function (first) and time (second)
-                setTimeout(() =>{
-                    //Here is where we call compareHands -- needs delay
-                    compareHands(this.textContent, computerChoice);
-                    //Update Images -- needs delay
-                    playerHand.src = `./assets/${this.textContent}.png`
-                    computerHand.src = `./assets/${computerChoice}.png`
-                    //reset button text to white -- needs delay
-                    option.style.color = "white";
-                    //re-enable buttons -- needs delay
-                    options.forEach(el =>{
-                       el.disabled = false;
-                    
-                   });
-                }, 2000)
-                //Shaking animation no delay
-                playerHand.style.animation = "shakePlayer 2s ease";
-                computerHand.style.animation = "shakeComputer 2s ease";
-                //current button color is red no delay
-                this.style.color = "rgb(39, 41, 68)";
-                //disable all buttons no delay
-                options.forEach(el => {
-                    el.disabled = true;
-
-                });
-                // endGame();
-                console.log("computer score:   ", cScore);
-                console.log("player Score:     ", pScore);
-            });
-            
+            myAttachEvent(option, "click", myListener);
+            console.log(option);
         });
-
+        console.log("hello");
 
        
 
@@ -132,43 +151,46 @@ const game = () => {
         }
 
     }
-    // const endGame = () =>{
-    //     const againBtn = document.querySelector('.result button');
-    //     const introScreen = document.querySelector('.intro');
-    //     const matchScreen = document.querySelector('.match');
-    //     const scoreScreen = document.querySelector('.score');
-    //     const resultScreen = document.querySelector('.result');
-    //     console.log("i'm running");
 
-    //     matchScreen.classList.remove('fadeIn');
-    //     scoreScreen.classList.remove('fadeIn');
-    //     resultScreen.classList.add('fadeIn');
-
-    //     againBtn.addEventListener('click', ()=> {
-    //         introScreen.classList.remove('fadeOut')
-    //         matchScreen.classList.add('fadeOut');
-    //         scoreScreen.classList.add('fadeOut');
-    //         resultScreen.classList.remove('fadeIn');
-    //         restartGame();
-    //     });
+    const endGame = () =>{
         
-    // };
+        const againBtn = document.querySelector('.result button');
+        const introScreen = document.querySelector('.intro');
+        const matchScreen = document.querySelector('.match');
+        const scoreScreen = document.querySelector('.score');
+        const resultScreen = document.querySelector('.result');
+        console.log("ending game");
+
+        matchScreen.classList.remove('fadeIn');
+        scoreScreen.classList.remove('fadeIn');
+        resultScreen.classList.add('fadeIn');
+
+        againBtn.addEventListener('click', ()=> {
+            introScreen.classList.remove('fadeOut')
+            matchScreen.classList.add('fadeOut');
+            scoreScreen.classList.add('fadeOut');
+            resultScreen.classList.remove('fadeIn');
+            restartGame();
+        });
+        
+    };
 
 
-    // const restartGame = () =>{
-    //     const winner = document.querySelector('.winner');
-    //     winner.textContent = "Choose an option.";
-    //     pScore = 0;
-    //     updateScore();
-    //     cScore = 0;
-    //     updateScore();
-    //     startGame();
-    // };
+    const restartGame = () =>{
+        const winner = document.querySelector('.winner');
+        winner.textContent = "Choose an option.";
+        pScore = 0;
+        updateScore();
+        cScore = 0;
+        updateScore();
+        startGame();
+        playMatch();
+    };
 
     //call all inner functions
     startGame();
     playMatch();
-    console.log("i/'m done");
+    console.log("i\'m done");
 
 };
 
